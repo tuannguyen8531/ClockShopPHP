47fd746053167b4a87fc96e89ec30ffe52f66d84
Nguyễn Dương Anh Tuấn <83623829+tuannguyen8531@users.noreply.github.com>
2023-11-16 18:08:22 +0700

    Add sidebar.

--- admin/baitap/sidebar.php
+++ admin/baitap/sidebar.php
@@ -12,7 +12,7 @@
         <!-- Logo -->
         <div class="header-left">
         <a href="index.php" class="logo">
-            <img src="../dist/file/jomashop_logo.7c0762d85d36b44f0c59.png" style="width: 90%; height: auto;"  alt="">
+            <img src="../../../dist/file/jomashop_logo.7c0762d85d36b44f0c59.png" style="width: 90%; height: auto;"  alt="">
         </a>
         </div>
         <!-- /Logo -->
@@ -44,42 +44,42 @@
                 <li class="submenu">
                     <a href="#"><i class="la la-dashboard"></i> <span>Quản lý</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="index.php">Quản trị Admin</a></li>
+                        <li><a href="../../index.php">Quản trị Admin</a></li>
                     </ul>
                 </li>
 
                 <li class="submenu">
                     <a href="#" class=""><i class="la la-user"></i> <span>Nhân Viên</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="employee.php">Thông tin nhân viên</a></li>
+                        <li><a href="../../employee.php">Thông tin nhân viên</a></li>
                     </ul>
                 </li>
                 <li class="submenu">
                     <a href="#" class=""><i class="la la-cube"></i> <span>Sản Phẩm</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
                         <li>
-                            <a href="./watches.php"> Watches</a>
+                            <a href="../../watches.php"> Watches</a>
                         </li>
                         <li>
-                            <a href="./category.php">Category</a>
+                            <a href="../../category.php">Category</a>
                         </li>
                         <li>
-                            <a href="./types.php">Types</a>
+                            <a href="../../types.php">Types</a>
                         </li>
                         <li>
-                            <a href="./movement.php">Movement</a>
+                            <a href="../../movement.php">Movement</a>
                         </li>
                         <li>
-                            <a href="./features.php">Features</a>
+                            <a href="../../features.php">Features</a>
                         </li>
                         <li>
-                            <a href="./caseshape.php">CaseShape</a>
+                            <a href="../../caseshape.php">CaseShape</a>
                         </li>
                         <li>
-                            <a href="./brand.php">Brand</a>
+                            <a href="../../brand.php">Brand</a>
                         </li>
                         <li>
-                            <a href="./style.php">Style</a>
+                            <a href="../../style.php">Style</a>
                         </li>
                     </ul>
                 </li>
@@ -87,13 +87,13 @@
                 <li class="submenu">
                     <a href="#" class=""><i class="la la-user"></i> <span>Khách Hàng</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="customer.php">Thông tin khách hàng</a></li>
+                        <li><a href="../../customer.php">Thông tin khách hàng</a></li>
                     </ul>
                 </li>
                 <li class="submenu">
                     <a href="#" class=""><i class="la la-money"></i> <span>Hóa Đơn</span> <span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="bill.php">Hóa đơn</a></li>
+                        <li><a href="../../bill.php">Hóa đơn</a></li>
                     </ul>
                 </li>
                 <li class="submenu">
@@ -106,12 +106,12 @@
                     <a href="#"><i class="la la-user"></i> <span>Hồ Sơ</span></a>
                 </li>
                 <li class="">
-                    <a href="./login.php"><i class="la la-key"></i><span>Đăng xuất</span></a>
+                    <a href="../../login.php"><i class="la la-key"></i><span>Đăng xuất</span></a>
                 </li>
                 <li class="submenu">
                     <a href="#"><i class="la la-book"></i><span>Bài tập</span><span class="menu-arrow"></span></a>
                     <ul style="display: none;">
-                        <li><a href="./baitap/php_form/baitap1.php">Bài tập</a></li>
+                        <li><a href="./php_form/baitap1.php">Bài tập</a></li>
                         <li><a href="#"></i>Thông tin cá nhân</a></li>
                     </ul>
                 </li>
